d53791d66c24968b0cf534b512b932d08c60806d
phazed <wtfux100@yahoo.com>
2026-05-29 10:37:32 -0700

    Create supabase-client.js

---
+++ supabase-client.js
@@ -0,0 +1,8 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+const SUPABASE_URL = "https://mxhdjaughcenjbndyzkf.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_TpSfosqGKbODi_BaxTQvtQ_77L385OG";
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
